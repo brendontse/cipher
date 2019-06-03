82a1d0754666781ce7a98db7a2ee93827140527b
Brendon Tse and Erik Myers <erik@>
2019-06-03 16:47:23 -0700

    create initial html and script.js

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var sentence = (prompt("Enter a sentence"));
+
+var first = sentence.charAt(0).toUpperCase();
+
+var last = sentence.charAt(sentence.length -1).toUpperCase()
+
+var reversed = last + first
+
+var combo = sentence + reversed
+
+var total = sentence.length
+
+var divided = total / 2
+
+var inputedNumber = parseInt(divided)
+
+var letter = sentence.charAt(inputedNumber)
+
+var final = letter + combo
+
+alert(reverseFinal)
